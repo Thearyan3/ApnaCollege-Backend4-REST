5ad354aca6b9efeef50777f674e559be89032b7e
Aryan <chaudharyaryan3456@gmail.com>
2025-02-06 23:08:42 +0530

    updated

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const methodOverride = require("method-override");
 const port = 8080;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
