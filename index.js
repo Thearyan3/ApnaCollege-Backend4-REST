6bb89b0c895fac55bbb9c3947c668da9e7912694
Aryan <chaudharyaryan3456@gmail.com>
2025-02-06 21:27:47 +0530

    updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({extended : true}));
 
@@ -12,14 +13,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id: uuidv4(),
         username: "Aryan",
         content: "He is starting trying his best"
     },
     {
+        id: uuidv4(),
         username: "Navya",
         content: "I Never Understood her."
     },
     {
+        id: uuidv4(),
         username: "Germany",
         content: "The Career is going to begin!"
     }
@@ -35,8 +39,15 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
-    posts.push({ username, content });
+    let id = uuidv4();
+    posts.push({ id, username, content });
     res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+    let { id } =  req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", { post });
 });
 
 app.listen(port, () => {
