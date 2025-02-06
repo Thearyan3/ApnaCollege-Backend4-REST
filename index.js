16008179886fd8252d4147b26da83b817639fd3f
Aryan <chaudharyaryan3456@gmail.com>
2025-02-06 20:29:26 +0530

    updated

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = requir("express");
+const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
 
+app.use(express.urlencoded({extended : true}));
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.set(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
