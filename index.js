84609fa623109e779cda7c782e576121f02ab19f
Aryan <chaudharyaryan3456@gmail.com>
2025-02-06 21:08:14 +0530

    updated

--- index.js
+++ index.js
@@ -8,7 +8,36 @@ app.use(express.urlencoded({extended : true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username: "Aryan",
+        content: "He is starting trying his best"
+    },
+    {
+        username: "Navya",
+        content: "I Never Understood her."
+    },
+    {
+        username: "Germany",
+        content: "The Career is going to begin!"
+    }
+];
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("form.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    posts.push({ username, content });
+    res.redirect("/posts");
+});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
